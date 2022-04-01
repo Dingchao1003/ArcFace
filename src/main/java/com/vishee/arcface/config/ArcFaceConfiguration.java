63036f886bf77d5c8b04d207076f9db1ae626b60
dingchao <dingchao1003@foxmailc.om>
2022-04-01 10:19:40 +0800

    branch update test

--- src/main/java/com/vishee/arcface/config/ArcFaceConfiguration.java
+++ src/main/java/com/vishee/arcface/config/ArcFaceConfiguration.java
@@ -48,7 +48,7 @@ public class ArcFaceConfiguration  {
     public ExecutorService crawlExecutorPool() {
         // 获取Java虚拟机的可用的处理器数，最佳线程个数，处理器数*2。根据实际情况调整
         int curSystemThreads = Runtime.getRuntime().availableProcessors() * 2;
-        System.out.println("------------系统可用线程池个数：" + curSystemThreads);
+        System.out.println("------------系统可用线程池个11111数：" + curSystemThreads);
         // 创建线程池
         return Executors.newFixedThreadPool(curSystemThreads);
 
